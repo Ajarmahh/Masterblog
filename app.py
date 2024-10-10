1e616c62f9264d06b70a59bc562aacff6c37bbff
Abdallah Al Sawae´er <ajarmah@Abdallahs-MacBook-Air.local>
2024-10-10 18:36:05 +0200

    Add /add route for creating new blog posts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -18,6 +18,29 @@ def index():
     with open('data.json', 'r') as json_file:
         blog_posts = json.load(json_file)
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('name')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        # Append the new post to the list
+        new_post = {
+            'id': len(blog_posts) + 1,  
+            'author': author,
+            'title': title,
+            'content': content
+        }
+        blog_posts.append(new_post)
+
+        # Save the updated list back to the JSON file
+        with open('data.json', 'w') as json_file:
+            json.dump(blog_posts, json_file, indent=4)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
