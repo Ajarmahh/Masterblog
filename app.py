6091766e605a537a59af0e175402f9f78661fdcf
Abdallah Al Sawae´er <ajarmah@Abdallahs-MacBook-Air.local>
2024-10-11 00:00:25 +0200

    Added the ability to update blog post

--- app.py
+++ app.py
@@ -57,5 +57,34 @@ def delete(post_id):
     return redirect(url_for('index'))
 
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update_post(post_id):
+    with open('data.json', 'r') as json_file:
+        blog_posts = json.load(json_file)
+
+    post = None
+    for p in blog_posts:
+        if p['id'] == post_id:
+            post = p
+            break
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        # Get the updated form data from the user
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        # Save the updated blog_posts list back to the data.json file
+        with open('data.json', 'w') as json_file:
+            json.dump(blog_posts, json_file, indent=4)
+
+        # Redirect back to the index page after updating
+        return redirect(url_for('index'))
+    return render_template('update.html', post=post)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
